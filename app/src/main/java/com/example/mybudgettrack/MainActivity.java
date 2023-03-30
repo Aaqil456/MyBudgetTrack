07b45a201fe2ba9fa44e1c6efd7540ba435cf4c0
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2023-03-30 17:20:55 +0800

    commit

--- app/src/main/java/com/example/mybudgettrack/MainActivity.java
+++ app/src/main/java/com/example/mybudgettrack/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.mybudgettrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.logout_item:
+                // Handle menu item 1 click
+                Toast.makeText(MainActivity.this, "rent", Toast.LENGTH_SHORT).show();
+                intent= new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
