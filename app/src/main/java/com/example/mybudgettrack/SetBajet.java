ca6433b15999d5b65eb031e3039244a2710a996c
Aaqil Ahamad <72489957+Aaqil456@users.noreply.github.com>
2023-05-28 02:33:21 +0800

    Improved SetBajet.java so that user can set their start date and end date for their bajet

--- app/src/main/java/com/example/mybudgettrack/SetBajet.java
+++ app/src/main/java/com/example/mybudgettrack/SetBajet.java
@@ -1,25 +1,29 @@
 package com.example.mybudgettrack;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class SetBajet extends AppCompatActivity {
     private EditText budgetInput;
-    private RadioGroup timePeriodSelection;
     private Button calculateButton;
     private TextView resultDisplay;
+    private TextView btnSetStartDate, btnSetEndDate;
+    private Calendar startDate, endDate;
 
     private DecimalFormat decimalFormat = new DecimalFormat("0.00");
     @Override
@@ -29,9 +33,24 @@ public class SetBajet extends AppCompatActivity {
 
         // Initialize views
         budgetInput = findViewById(R.id.edittext_budget);
-        timePeriodSelection = findViewById(R.id.radiogroup_duration);
         calculateButton = findViewById(R.id.button_calculate);
         resultDisplay = findViewById(R.id.result_display);
+        btnSetStartDate = findViewById(R.id.btnSetStartDate);
+        btnSetEndDate = findViewById(R.id.btnSetEndDate);
+
+        btnSetStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(true);
+            }
+        });
+
+        btnSetEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog(false);
+            }
+        });
 
 
 
@@ -48,21 +67,16 @@ public class SetBajet extends AppCompatActivity {
                 double budget = Double.parseDouble(budgetString);
 
                 // Get time period selection
-                int checkedRadioButtonId = timePeriodSelection.getCheckedRadioButtonId();
-                if (checkedRadioButtonId == -1) {
-                    resultDisplay.setText("Please select a time period.");
-                    return;
-                }
-                RadioButton checkedRadioButton = findViewById(checkedRadioButtonId);
-                String timePeriod = checkedRadioButton.getText().toString();
 
                 // Calculate daily expenditure
-                double dailyExpenditure;
+                double dailyExpenditure=budget/calculateNumberOfDays();
+
+                /*
                 if (timePeriod.equals("Week")) {
                     dailyExpenditure = budget / 7;
                 } else {
                     dailyExpenditure = budget / 30;
-                }
+                }*/
 
                 // Set the dailyExpenditure in the Singleton
                 DailyExpenditureSingleton.getInstance().setDailyExpenditure(dailyExpenditure);
@@ -80,6 +94,71 @@ public class SetBajet extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void setStartDate(View view) {
+        showDatePickerDialog(true);
+    }
+
+    public void setEndDate(View view) {
+        showDatePickerDialog(false);
+    }
+
+    public int calculateNumberOfDays() {
+        if (startDate != null && endDate != null) {
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+            String startDateString = format.format(startDate.getTime());
+            String endDateString = format.format(endDate.getTime());
+
+            try {
+                Date startDate = format.parse(startDateString);
+                Date endDate = format.parse(endDateString);
+
+                long difference = endDate.getTime() - startDate.getTime();
+                int numberOfDays = (int) (difference / (1000 * 60 * 60 * 24));
+
+                return numberOfDays;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return 0; // Return 0 if start or end date is null or in case of any error
+    }
+
+
+    private void showDatePickerDialog(final boolean isStartDate) {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar selectedDate = Calendar.getInstance();
+                        selectedDate.set(year, month, dayOfMonth);
+
+                        if (isStartDate) {
+                            startDate = selectedDate;
+                            btnSetStartDate.setText(formatDate(selectedDate));
+                        } else {
+                            endDate = selectedDate;
+                            btnSetEndDate.setText(formatDate(selectedDate));
+                        }
+                    }
+                },
+                year, month, day);
+
+        datePickerDialog.show();
+    }
+
+    private String formatDate(Calendar calendar) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(calendar.getTime());
     }
 
     @Override
